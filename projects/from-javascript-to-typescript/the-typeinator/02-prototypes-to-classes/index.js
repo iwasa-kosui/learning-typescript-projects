f766c95a0e4a455719b2cc4c1d4858b23eb3bff5
iwasa-kosui <dev@kosui.me>
2024-07-02 12:54:47 +0900

    feat: the-typeinator

--- projects/from-javascript-to-typescript/the-typeinator/02-prototypes-to-classes/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/02-prototypes-to-classes/index.js
@@ -1,5 +1,66 @@
-// Put your Robot and Humanoid classes here! ✨
-// See ./original.js for their older JavaScript code.
+class Robot {
+	constructor(name, abilities) {
+		/** @type {string} */
+		this.name = name;
+		/** @type {string[]} */
+		this.abilities = abilities;
+		/** @type {number} */
+		this.power = 100;
+	}
+
+	announce() {
+		console.log("Greetings. I am " + this.name + ".");
+		for (const ability of this.abilities) {
+			console.log(`I am able to ${ability}.`);
+		}
+	}
+
+	/**
+	 * @param {number} amount
+	 */
+	charge(amount) {
+		if (this.power < 100) {
+			this.power = Math.min(this.power + amount, 100);
+			console.log(`Recharged power supplies to ${this.power}.`);
+		}
+
+		if (this.power === 100) {
+			console.log("I am at optimal operational capacity.");
+		}
+	}
+
+	/**
+	 * @param {number} distance
+	 */
+	move(distance) {
+		if (this.power < distance) {
+			console.log(`I do not have enough power to move ${distance} units.`);
+		} else {
+			console.log(`Moving ${distance} units.`);
+			this.power -= distance;
+		}
+	}
+}
+
+class Humanoid extends Robot {
+	/**
+	 *
+	 * @param {string} name
+	 * @param {string[]} abilities
+	 * @param {string} catchphrase
+	 */
+	constructor(name, abilities, catchphrase) {
+		super(name, abilities);
+
+		/** @readonly @type {string} */
+		this.catchphrase = catchphrase;
+	}
+
+	announce() {
+		super.announce();
+		console.log(` > ${this.catchphrase} <`);
+	}
+}
 
 module.exports.Humanoid = Humanoid;
 module.exports.Robot = Robot;
--- projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
@@ -1,5 +1,32 @@
-// Put your checkEmotion and speak functions here! ✨
-// See ./original.js for their older JavaScript code.
+/**
+ * @param {number} ms
+ * @returns {Promise<void>}
+ */
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+/**
+ * @param {Set<string>} knownEmotions
+ * @param {string} emotion
+ * @returns {Promise<boolean>}
+ */
+const checkEmotion = async (knownEmotions, emotion) => {
+	await sleep(1000);
+	return knownEmotions.has(emotion);
+};
+
+/**
+ * @param {Set<string>} knownEmotions
+ * @param {string} newEmotion
+ * @param {string} phrase
+ * @returns {Promise<string>}
+ */
+const speak = async (knownEmotions, newEmotion, phrase) => {
+	const hasEmotion = await checkEmotion(knownEmotions, newEmotion);
+	if (!hasEmotion) {
+		throw new Error(`Does not compute. I do not understand ${newEmotion}.`);
+	}
+	return `"${phrase}" (${newEmotion})`;
+};
 
 module.exports.checkEmotion = checkEmotion;
 module.exports.speak = speak;
